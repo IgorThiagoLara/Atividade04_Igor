da34084d2ad167b4949e5546dc4548fc8fd1240e
IgorThiagoLara <163324764+IgorThiagoLara@users.noreply.github.com>
2024-04-11 22:04:16 -0300

    Update exercicio01.py

--- exercicio01.py
+++ exercicio01.py
@@ -69,4 +69,44 @@ for i in range(1,11):
 print(f"o número de pares é: {len(listaPar)}")
 print(f"o número de ímpares é: {len(listaImpar)}")'''
 
+'''6. Utilizando a estrutura de repetição for, faça um programa que receba 10
+números e conte quantos deles estão no intervalo [10,20] e quantos
+deles estão fora do intervalo.'''
+'''print("Programa que contabiliza quantos números digitados estão entre o intervalo de 10 e 20.")
+print(100*"-")
+numerosInterv = []
+for i in range(1,11):
+    numeros = int(input("Digite um número: "))
+    if numeros <0:
+        print("Número considerado inválido para o teste, tente novamente!")
+        continue
+    if numeros >=10 and numeros<=20:
+        numerosInterv.append(numeros)
+print(f"Você digitou {len(numerosInterv)} números entre 10 e 20")
+print(100*"-")'''
 
+#7. Faça um programa que calcule e mostre a soma dos 50 primeiros números pares.
+'''somaPares = 0
+pares = []
+for i in range(1,101):
+    if i%2==0:
+        print(f"O número {i} é par")
+        pares.append(i)
+        somaPares= somaPares+i
+print(len(pares))
+print(f"A soma de todos os 50 primeiros números pares é: {somaPares}")'''
+
+#8. Faça um algoritmo que leia um número positivo e imprima seus divisores
+'''lista = []
+while True:
+    numero = int(input("Digite um número postivo: "))
+    if numero<0:
+       print("Número inválido, tente novamente!")
+       continue
+    else:
+        break
+for i in range(1,numero+1):
+    div = numero%i
+    if div ==0:
+        lista.append(i)
+print(f"Esses são os divisores do número {numero}: {lista}")'''
